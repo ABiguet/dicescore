21859d8c21489f1f204e99112f7b8f596c54abb8
Aurelien Biguet <10219840+ABiguet@users.noreply.github.com>
2025-10-02 22:50:25 +0200

    fix: corriger la réponse de l'ajout de joueur et améliorer la gestion des identifiants dans le tableau des joueurs

--- src/routes/game.js
+++ src/routes/game.js
@@ -18,12 +18,15 @@ router.post('/add-player-to-game', (req, res) => {
   const existingPlayer = req.session.players.find(p => p.id === req.body.player.id);
   if (!existingPlayer) {
     req.session.players.push(req.body.player); // Ajouter un joueur
+    res.sendStatus(200);
   }
-  res.sendStatus(200);
+  else {
+    res.sendStatus(409);
+  }
 });
 
 router.delete('/remove-player-from-game/:id', (req, res) => {
-  const playerId = parseInt(req.params.id, 10); // Convertir en nombre
+  const playerId = req.params.id;
   if (req.session.players) {
     req.session.players = req.session.players.filter(p => p.id !== playerId);
   }
